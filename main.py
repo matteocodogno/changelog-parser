98fbf7779a8397f6fbfd473d7cf09f135f2074f7
Matteo Codogno <matteo.codogno@welld.ch>
2019-01-24 14:36:10 +0100

    Add argument to specify the changelog filename

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from markdown_to_json.markdown_to_json import Renderer, CMarkASTNester
 from markdown_to_json.scripts.md_to_json import get_markdown_ast
 
 
-def convert_changelog_to_json():
+def convert_changelog_to_json(file_name):
     nester = CMarkASTNester()
     renderer = Renderer()
-    ast = get_markdown_ast('./CHANGELOG.md')
+    ast = get_markdown_ast(file_name)
     nested = nester.nest(ast)
 
     return renderer.stringify_dict(nested)
@@ -36,10 +36,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Extract a specific release from changelog')
     parser.add_argument('--project-version', default='Unreleased',
                         help='Project version to be extracted (default: Unreleased)')
+    parser.add_argument('--filename', default='./CHANGELOG.md',
+                        help='Path to CHANGELOG file')
 
     args = parser.parse_args()
 
-    changelog_dict, = list(convert_changelog_to_json().values())
+    changelog_dict, = list(convert_changelog_to_json(args.filename).values())
     release_dict = extract_release_by_name(changelog_dict, args.project_version)
     print(release_dict_to_markdown(release_dict))
 
